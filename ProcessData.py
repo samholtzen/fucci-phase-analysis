cc0130b63c520904b4a3e70db4bf37166568dde6
samholtzen <saho2901@colorado.edu>
2021-11-05 16:44:56 +0000

    added count_phase_frames function

--- ProcessData.py
+++ ProcessData.py
@@ -18,7 +18,7 @@ def main():
     
 def trim_array(cell_phases, media_frame):
     
-    """
+    '''
     Purpose:
         Input two numpy arrays of fluorescent protein data and trim them
         to just look at data after media change frame.
@@ -31,7 +31,7 @@ def trim_array(cell_phases, media_frame):
         particular frame
     
     
-    """
+    '''
     media_ind = media_frame-1
     mC_after_change = mCherrydata[:,media_ind:]
     mV_after_change = mVenusdata[:,media_ind:]
@@ -40,7 +40,7 @@ def trim_array(cell_phases, media_frame):
 
 
 def count_phase_frames(cell_phases):
-    """
+    '''
     Purpose:
         To count the number of frames a cell spends in each phase after
         changing the media.
@@ -54,13 +54,47 @@ def count_phase_frames(cell_phases):
 
 
     Outputs:
-        A mixed character/integer vector containing in the first row a cell's
-        phase, in the second row the cell's time spent in that phase
-    """
+        Three lists of lists, containing the time spent in each phase in the
+        sublist, with each element in the larger list corresponding to the
+        tracks
+    '''
+    all_G1_lengths = []
+    all_S_lengths = []
+    all_G2_lengths = []
+    
+    for cell in cell_phases:
+        
+        # Convert the numpy array to one long string
+        cellstr_temp = ''.join(cell_phases[cell])
+        
+        # Split the array into individual cells by split string at 'M'
+        cell_split = cellstr_temp.split('M')
+        
+        
+        G1_lengths = []
+        S_lengths = []
+        G2_lengths = []
+        
+        for i in cell_phases:
+            
+            # Loop through and find the number of instances of "G1"
+            G1_length = cell_split[i].count('G1')
+            G1_lengths.append(G1_length)
+            
+            S_length = cell_split[i].count('S')
+            S_lengths.append(S_length)
+            
+            G2_length = cell_split[i].count('G2')
+            G2_lengths.append(G2_length)
+        
+    all_G1_lengths.append(G1_lengths)
+    all_S_lengths.append(S_lengths)
+    all_G2_lengths.append(G2_lengths)
+    
+    return all_G1_lengths, all_S_lengths, all_G2_lengths
+    
+
+def tpa_timing(cell_phases, media_frame):
     
     
-    
-    
-    
-    return 
     
