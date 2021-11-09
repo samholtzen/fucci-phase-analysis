23c7350cb2179617230a3545d02da6742e52b5a3
samholtzen <saho2901@colorado.edu>
2021-11-09 15:52:20 +0000

    sam_plotting is a misnomer -- this commit is for adding writing the structure to .csv

--- ProcessData.py
+++ ProcessData.py
@@ -49,8 +49,10 @@ def count_phase_frames(cell_phases, media_frame):
         # Convert the numpy array to one long string
         cellstr_temp = ''.join(cell_phases[cell])
         
+        # Only look at cells after the media change
         cellstr_temp = cellstr_temp[media_frame:]
-        # Split the array into individual cells by split string at 'M'
+        
+        # Split the array into individual cell traces by split string at 'M'
         cell_split = cellstr_temp.split('M')
         
         
@@ -62,13 +64,13 @@ def count_phase_frames(cell_phases, media_frame):
             
             # Loop through and find the number of instances of "G1"
             G1_length = cell_split[i].count('G1')
-            G1_lengths.append(G1_length)
+            G1_lengths.append(G1_length/5)
             
             S_length = cell_split[i].count('S')
-            S_lengths.append(S_length)
+            S_lengths.append(S_length/5)
             
             G2_length = cell_split[i].count('G2')
-            G2_lengths.append(G2_length)
+            G2_lengths.append(G2_length/5)
         
     all_G1_lengths.append(G1_lengths)
     all_S_lengths.append(S_lengths)
@@ -110,7 +112,7 @@ def media_timing(cell_phases, media_frame):
         
         while phase_before == phase_at_change:
             
-            if phase_before > 2:
+            if frame_to_check > 2:
                 
                 phase_before = cell[frame_to_check]
                 time_in_phase += 1
@@ -120,12 +122,13 @@ def media_timing(cell_phases, media_frame):
                 
                 break
         
-        time_in_phase_at_change.append(time_in_phase)
+        time_in_phase_at_change.append(time_in_phase/5)
     
     return cell_phase_at_change, time_in_phase_at_change
 
 
-def get_cell_G1_stats(cell_phase_at_change, time_in_phase_at_change, all_G1_lengths):
+def get_daughter_stats(cell_phase_at_change, time_in_phase_at_change, 
+                   all_G1_lengths, all_G2_lengths, all_S_lengths):
         '''
     Purpose:
         To bin cells based on their fate and correlate this to their time in
@@ -147,11 +150,13 @@ def get_cell_G1_stats(cell_phase_at_change, time_in_phase_at_change, all_G1_leng
         Returning an n x 3 array, with each row corresponding to a cell
         column 1 is the phase of the cell at media change
         column 2 is frames into the phase the cell was before media change
-        column 3 is how long the daughter G1 is 
+        column 3 is how long the daughter G1 is
+        colum 4 is how long the daughter S is
+        column 5 is how long the daughter G2 is
         
     '''
     
-    all_cell_G1_stats = []
+    daughter_cell_stats = []
     
     for i in range(len(cell_phase_at_change)):
         
@@ -161,115 +166,82 @@ def get_cell_G1_stats(cell_phase_at_change, time_in_phase_at_change, all_G1_leng
         if phase_temp == 'G1':
             
             G1_temp = all_G1_lengths[1]
+            S_temp = all_S_lengths[0]
+            G2_temp = all_G2_lengths[0]
             
-        else:
+        elif phase_temp == 'S':
             
             G1_temp = all_G1_lengths[0]
+            S_temp = all_S_lengths[1]
+            G2_temp = all_G2_lengths[0]
             
-        cell_temp = [phase_temp,time_temp_G1_temp]
+        elif phase_temp == 'G2':
+            
+            G1_temp = all_G1_lengths[0]
+            S_temp = all_S_lengths[0]
+            G2_temp = all_G2_lengths[1]
+            
+            
+        cell_temp = [phase_temp,time_temp,G1_temp,S_temp,G2_temp]
         
-        all_cells_G1_stats.append(cell_temp)
+        daughter_cell_stats.append(cell_temp)
         
+    return daughter_cell_stats
 
-    return all_cells_G1_stats
-    
-    
-def get_cell_S_stats(cell_phase_at_change, time_in_phase_at_change, all_S_lengths):
-        '''
+def convert_phase_to_time(daughter_cell_stats):
+    '''
     Purpose:
-        To bin cells based on their fate and correlate this to their time in
-        treatment media.
+        To convert Phase and Time into Phase into a pseudotime for plotting
         
     
     Inputs:
-        A list of strings with each element corresponding to the cell's phase
-        at change
-        
-        A list of ints with each element corresponding to the cell's time 
-        spent in the phase
-        
-        A list of lists containing how long each cell spends in the daughter
-        cell Ss
+        All of the cell stats, which houses the phase, the 
 
 
     Outputs:
-        Returning an n x 3 array, with each row corresponding to a cell
-        column 1 is the phase of the cell at media change
-        column 2 is frames into the phase the cell was before media change
-        column 3 is how long the daughter S is 
+        Returning an n x 4 array, with each row corresponding to a cell
+        column 1 is the time into the cell cycle a cell is at
+        the time of media change
         
+        column 2 is how long the daughter G1 is 
+        column 3 is how long the daughter S is
+        column 4 is how long the daughter G2 is
     '''
     
-    all_cell_S_stats = []
+    daughter_cell_to_plot = []
     
-    for i in range(len(cell_phase_at_change)):
+    for daughters in all_cell_stats:
         
-        phase_temp = cell_phase_at_change[i]
-        time_temp = time_in_phase_at_change[i]
+        phase = daughters[0]
+        time = daughters[1]
+        lengths = daughters[2:]
         
-        if phase_temp == 'S':
-            
-            G1_temp = all_S_lengths[1]
-            
-        else:
-            
-            G1_temp = all_S_lengths[0]
-            
-        cell_temp = [phase_temp,time_temp_S_temp]
+        if phase == 'G1':
+            phase_out = time
+        elif phase == 'S':
+            phase_out = time + 4
+        elif phase == 'G2':
+            phase_out = time + 9
         
-        all_cells_S_stats.append(cell_temp)
+        time_convert_temp = [phase_out,lengths]
         
+        daughter_cell_to_plot.append(time_convert_temp)
+        
+            
+    return daughter_cell_to_plot
 
-    return all_cells_S_stats
 
-
-def get_cell_G2_stats(cell_phase_at_change, time_in_phase_at_change, all_G2_lengths):
-        '''
-    Purpose:
-        To bin cells based on their fate and correlate this to their time in
-        treatment media.
-        
+def write_list_csv(daughter_cell_stats):
     
-    Inputs:
-        A list of strings with each element corresponding to the cell's phase
-        at change
-        
-        A list of ints with each element corresponding to the cell's time 
-        spent in the phase
-        
-        A list of lists containing how long each cell spends in the daughter
-        cell G2s
-
-
-    Outputs:
-        Returning an n x 3 array, with each row corresponding to a cell
-        column 1 is the phase of the cell at media change
-        column 2 is frames into the phase the cell was before media change
-        column 3 is how long the daughter G2 is 
-        
-    '''
+    daughter_cell_stats_df = pd.DataFrame(daughter_cell_stats,
+                                          columns=['Phase','Time',
+                                                   'Daughter_G1','Daughter_S',
+                                                   'Daughter_G2'])
     
-    all_cell_G2_stats = []
+    daughter_cell_stats_df.to_csv('daughter_cell_phases.csv',index=False, header=True)
     
-    for i in range(len(cell_phase_at_change)):
-        
-        phase_temp = cell_phase_at_change[i]
-        time_temp = time_in_phase_at_change[i]
-        
-        if phase_temp == 'G2':
-            
-            G1_temp = all_G2_lengths[1]
-            
-        else:
-            
-            G1_temp = all_G2_lengths[0]
-            
-        cell_temp = [phase_temp,time_temp_G2_temp]
-        
-        all_cells_G2_stats.append(cell_temp)
-        
-
-    return all_cells_G2_stats
+    
+    return None
 
 
 if __name__ == '__main__':
