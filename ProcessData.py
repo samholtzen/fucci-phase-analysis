80df1c039ca5c44055cb5e5def30a54e35f87fec
saisam17 <saisam17@gmail.com>
2021-11-14 11:55:08 -0700

    Added functions to normalize data and updated utils.py

--- ProcessData.py
+++ ProcessData.py
@@ -1,43 +1,45 @@
 import utils as ut
-
+import normalize_utils
 
 
 def main():
-    
-    
-        '''
-    This function runs main arguments.
+    """
+This function runs main arguments.
 
-    Parameters
-    —---------
-    a : str
-    First argument is the filename/path to mCherry file
-    B : str
-    Second argument is the filename/path to mVenus file
-    '''
+Parameters
+—---------
+a : str
+First argument is the filename/path to mCherry file
+B : str
+Second argument is the filename/path to mVenus file
+"""
 
     # Call input_parser to parse commandline inputs
-    args = input_parser()
-    
+    args = ut.input_parser()
+
     # Get data from file using file_reader
     mCherrydata = ut.file_reader(args.mCherryfilename)
-    
+    #print(len(mCherrydata))
+
     # Get data from file using file_reader
     mVenusdata = ut.file_reader(args.mVenusfilename)
-    
+    #print(len(mVenusdata))
+
+    normalize_utils.find_mitosis_event(mCherrydata, mVenusdata)
+
     #Find derivative (rate of change) for each cell line
     mCherryDerivative = ut.get_derivative(mCherrydata)
     mVenusDerivative = ut.get_derivative(mVenusdata)
-    
+
     #Find ratio between mCherry and mVenus for each cell line
     CherryToVenusRatio = ut.get_ratio(mCherrydata, mVenusdata)
-    
+
     #Get the media_change frame from the input
     media_frame = args.mediaframe
-    
-    
-    
-    
+
+
+
+
 
 
 
